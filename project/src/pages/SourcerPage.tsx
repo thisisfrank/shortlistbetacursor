0b1a2a08973dfed49f38485b29c3cfb4d3b59713
Frank <thisisfrankgonzalez@gmail.com>
2025-07-23 02:09:04 -0700

    COMPLETE FLATTEN: Remove duplicate project/ directory - now using single clean structure

--- project/src/pages/SourcerPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SourcerDashboardView } from '../components/sourcer/SourcerDashboardView';
-
-export const SourcerPage: React.FC = () => {
-  return <SourcerDashboardView />;
-};
